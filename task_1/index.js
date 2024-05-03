a0e97736aa5df308ede09671a6c3f32beabe7324
DanilGolu <dokers190rus@mail.ru>
2024-05-03 22:23:39 +0300

    process

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,30 @@
 import {encoded, translations} from './data.js'
+function decryptFields(encoded, translations) {
+    let decryptedList = [];
 
-console.log("Let's rock")
+    encoded.forEach(item => {
+        let decryptedItem = {};
+        for (let key in item) {
+            if (key.endsWith("Id") && translations[item[key]] !== undefined) {
+                decryptedItem[key] = translations[item[key]];
+            } else if (!['groupId', 'service', 'formatSize', 'ca'].includes(key)) {
+                decryptedItem[key] = item[key];
+            } else {
+                decryptedItem[key] = item[key];
+            }
+        }
+        decryptedList.push(decryptedItem);
+    });
+
+    return decryptedList;
+}
+
+
+
+console.log(decryptFields(encoded, translations));
+
+/*
+
+ console.log("Let's rock")
 console.log(encoded, translations)
-
-
-
-// console.log(decoded)
+*/
--- task_2/src/vite-env.d.ts
+++ task_2/src/vite-env.d.ts
@@ -1,2 +1,19 @@
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
+
+declare module '*.scss' {
+    const css: { [key: string]: string };
+    export default css;
+  }
+  declare module '*.sass' {
+    const css: { [key: string]: string };
+    export default css;
+  }
+  declare module 'react-markup';
+  declare module '*.webp';
+  declare module '*.png';
+  declare module '*.jpg';
+  declare module '*.jpeg';
+  declare module '*.svelte';
+  declare module '*.json';
+  declare module '*.js';
+  declare module "svelte"
+  
